0d435e3e2dde33bffd2dffb5ba9b8dbc7f9bda9e
Fatimah <Nyzak1@outlook.sa>
2023-08-02 00:16:23 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+     MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello Worldooo'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          colors: [Color(0xff9c27b0), Color(0xfff44336)],
+          stops: [0, 1],
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+        )
+          ),
+          child: const Center(
+            child: Text('Hello Worldooo', style: TextStyle(
+              color: Colors.white,
+              fontSize: 28,
+            )),
+          ),
         ),
       ),
     ),
