57d5b257ff74cd0b12a832b4cf49cf592f215274
Fatimah <Nyzak1@outlook.sa>
2023-08-02 11:42:12 +0300

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-     MaterialApp(
+     const MaterialApp(
       home: Scaffold(
-        body: Container(
+        body: GradientContainer(), //see the widget class below
+        ),
+      ),
+    );
+}
+
+class GradientContainer extends StatelessWidget {
+  
+  //const GradientContainer({key}): super(key: key);
+  const GradientContainer({super.key}); //this is a short code for the previos line of code
+  @override
+  Widget build(context) {
+    return Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
-          colors: [Color(0xff9c27b0), Color(0xfff44336)],
+          colors: [
+            Color(0xff9c27b0),
+            Color(0xfff44336)],
           stops: [0, 1],
           begin: Alignment.topRight,
           end: Alignment.bottomLeft,
@@ -19,8 +33,6 @@ void main() {
               fontSize: 28,
             )),
           ),
-        ),
-      ),
-    ),
-  );
+        );
+  }
 }
