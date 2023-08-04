d30a06920f99cd7b0336d1b50ef2ddabc43395e9
Fatimah <Nyzak1@outlook.sa>
2023-08-04 20:55:57 +0300

    new process

---
+++ lib/gradient_container.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  
+  //const GradientContainer({key}): super(key: key);
+  const GradientContainer({super.key}); //this is a short code for the previos line of code
+  @override
+  Widget build(context) {
+    return Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          colors: [
+            Color(0xff9c27b0),
+            Color(0xfff44336)
+            ],
+          stops: [0, 1],
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+        )
+          ),
+          child: const Center(
+            child: Text('Hello Worldooo', style: TextStyle(
+              color: Colors.white,
+              fontSize: 28,
+            )),
+          ),
+        );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,12 @@
 import 'package:flutter/material.dart';
+import 'gradient_container.dart';
 
 void main() {
   runApp(
      const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(), //see the widget class below
+        body: GradientContainer(), //see the widget class from gradient_container.dart
         ),
       ),
     );
-}
-
-class GradientContainer extends StatelessWidget {
-  
-  //const GradientContainer({key}): super(key: key);
-  const GradientContainer({super.key}); //this is a short code for the previos line of code
-  @override
-  Widget build(context) {
-    return Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-          colors: [
-            Color(0xff9c27b0),
-            Color(0xfff44336)],
-          stops: [0, 1],
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-        )
-          ),
-          child: const Center(
-            child: Text('Hello Worldooo', style: TextStyle(
-              color: Colors.white,
-              fontSize: 28,
-            )),
-          ),
-        );
-  }
 }
