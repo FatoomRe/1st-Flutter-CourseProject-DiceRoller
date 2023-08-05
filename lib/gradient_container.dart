7c4949d142979f3eb1780fe17afeb72d868e6d90
Fatimah <Nyzak1@outlook.sa>
2023-08-05 16:46:47 +0300

    v46 Styling Buttons & Working with Padding

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -9,7 +9,7 @@ class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
 
-  void rollDice (){
+  void rollDice() {
     //----
   }
 
@@ -25,13 +25,22 @@ class GradientContainer extends StatelessWidget {
       )),
       child: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(
               'assets/images/dice-5.png',
               width: 200,
             ),
+            const SizedBox(height: 30,), //مسافة بين الزر والمربع
             TextButton(
               onPressed: rollDice,
+              style: TextButton.styleFrom(
+                // padding: const EdgeInsets.only(top: 20),
+                //طريقة ثانية للمسافة
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(
+                    fontSize: 28,
+                  )),
               child: const Text('Roll Dice'),
             )
           ],
