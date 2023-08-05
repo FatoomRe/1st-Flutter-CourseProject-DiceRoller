994759bfe5162b4d3941ca83a260d39cc1fd15b2
Fatimah <Nyzak1@outlook.sa>
2023-08-05 16:23:33 +0300

    v45 adding button

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,31 +1,43 @@
-
 import 'package:flutter/material.dart';
 
 const startAlignement = Alignment.topRight;
 const endAlignement = Alignment.bottomLeft;
 
 class GradientContainer extends StatelessWidget {
-  
-  const GradientContainer(this.color1, this.color2, {super.key}); 
-  
+  const GradientContainer(this.color1, this.color2, {super.key});
+
   final Color color1;
   final Color color2;
+
+  void rollDice (){
+    //----
+  }
 
   @override
   Widget build(context) {
     return Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-          colors: [color1, color2],
-          stops:  const [0, 1],
-          begin: startAlignement,
-          end: endAlignement,
-        )
-          ),
-          child: Center(
-            child: Image.asset('assets/images/dice-5.png', width: 200,),
-          ),
-        );
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: [color1, color2],
+        stops: const [0, 1],
+        begin: startAlignement,
+        end: endAlignement,
+      )),
+      child: Center(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/dice-5.png',
+              width: 200,
+            ),
+            TextButton(
+              onPressed: rollDice,
+              child: const Text('Roll Dice'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
 
